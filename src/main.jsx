3be16b70428f81eb94346bf362960d49d385c394
SUShi <24042498@student.hhs.nl>
2026-03-30 13:21:05 +0200

    ft: web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
       <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 )
